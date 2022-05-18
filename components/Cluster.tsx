28e17542682a52c94c0a495f6f858514e49af904
karukan029 <karukan029@gmail.com>
2022-05-19 06:53:55 +0900

    add Cluster

--- components/Cluster.tsx
+++ components/Cluster.tsx
@@ -9,7 +9,36 @@ type Props = {
 type StyleProps = {
   // 子要素をコンテンツ幅に基づいて中央揃えにさせるか
   gap?: string;
+  justifyContent?: justifyContent;
+  alignItems?: alignItems;
 };
+
+type justifyContent =
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'left'
+  | 'right';
+
+type alignItems =
+  | 'normal'
+  | 'stretch'
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'baseline'
+  | 'first baseline'
+  | 'last baseline'
+  | 'safe center'
+  | 'unsafe center'
+  | 'inherit'
+  | 'initial'
+  | 'revert'
+  | 'unset';
 
 const Cluster: FC<Props> = (props) => (
   <div css={clusterStyle(props)}>{props.children}</div>
@@ -19,6 +48,10 @@ const clusterStyle = (props: StyleProps) => css`
   display: flex;
   flex-wrap: wrap;
   gap: ${props.gap ? props.gap : '1rem'};
+  /* 水平軸 */
+  justify-content: ${props.justifyContent ? props.justifyContent : 'center'}
+  /* 垂直軸 */
+  align-items: ${props.alignItems ? props.alignItems : 'center'}
 `;
 
 export default Cluster;
